24422929faa70345d61b8680bf7a9c84069f3349
ket_ein17 <englandstein@gmail.com>
2025-09-06 00:20:25 +0700

    Add exception handler

--- src/main/java/security/instagram/controller/DoSomethingController.java
+++ src/main/java/security/instagram/controller/DoSomethingController.java
@@ -1,15 +1,20 @@
 package security.instagram.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import security.instagram.config.response_data.ResponseBuilder;
+import security.instagram.utils.exception.InvalidRequestException;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/v1/api")
 public class DoSomethingController {
     @GetMapping("/test")
-    public ResponseEntity<String> doSomething() {
-        return ResponseEntity.ok("Do something");
+    public ResponseEntity<?> doSomething(HttpServletRequest request) {
+        return ResponseBuilder.success("Do Something", "SUCCESS", request);
     }
 }
